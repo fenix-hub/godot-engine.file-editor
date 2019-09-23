82765d7d9665f53748699fbecef945a4a09c2406
fenix-hub <n.santilio97@gmail.com>
2019-09-23 15:06:43 +0200

    version 0.1.1

---
+++ addons/file-editor/scripts/file-editor.gd
@@ -0,0 +1,12 @@
+tool
+extends EditorPlugin
+
+var doc
+
+func _enter_tree():
+	doc = preload("../scenes/FileEditor.tscn").instance()
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BR,doc)
+
+func _exit_tree():
+	remove_control_from_docks(doc)
+	doc.queue_free()
