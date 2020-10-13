c1242cb1600f3e73be9a6ac9eca6c9ccad4a0341
Nicolò Santilio <n.santilio97@gmail.com>
2020-10-13 17:17:22 +0200

    Update file for #25

--- addons/file-editor/scripts/LastOpenedFiles.gd
+++ addons/file-editor/scripts/LastOpenedFiles.gd
@@ -3,6 +3,8 @@ extends Node
 
 const lastopenedfile_path : String = "res://addons/file-editor/lastopenedfiles.lastcfg"
 
+var editor_plugin : EditorPlugin = EditorPlugin.new()
+
 func _ready():
 	pass
 
@@ -40,3 +42,6 @@ func store_editor_fonts(file_name : String, font_path : String):
 	file.load(lastopenedfile_path)
 	file.set_value("Fonts",file_name,font_path)
 	file.save(lastopenedfile_path)
+
+func get_editor_font():
+	return editor_plugin.get_editor_interface().get_editor_settings().get_setting("interface/editor/code_font")
